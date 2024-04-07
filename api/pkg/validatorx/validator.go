7dc1e1b6c0cbfb2bec0cb6b4f08676b84ba244d8
董凤龙 <1024071953@qq.com>
2024-04-07 23:42:17 +0800

    1.自定义校验规则，自定义错误信息

--- api/pkg/validatorx/validator.go
+++ api/pkg/validatorx/validator.go
@@ -1,6 +1,7 @@
 package validatorx
 
 import (
+	"fmt"
 	"github.com/go-playground/locales/en"
 	"github.com/go-playground/locales/zh"
 	ut "github.com/go-playground/universal-translator"
@@ -8,6 +9,7 @@ import (
 	enTranslations "github.com/go-playground/validator/v10/translations/en"
 	zhTranslations "github.com/go-playground/validator/v10/translations/zh"
 	"reflect"
+	"regexp"
 )
 
 var Validator *validator.Validate
@@ -22,6 +24,16 @@ func init() {
 	//创建验证器
 	Validator = validator.New()
 
+	// 注册自定义校验规则
+	Validator.RegisterValidation("mobile", MobileValidation)
+	// 注册自定义错误
+	Validator.RegisterTranslation("mobile", Trans, func(ut ut.Translator) error {
+		return ut.Add("mobile", "{0}格式错误", true)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("mobile", fe.Field())
+		return t
+	})
+
 	//翻译器注册到validator
 	switch locale {
 	case "zh":
@@ -35,5 +47,26 @@ func init() {
 		Validator.RegisterTagNameFunc(func(field reflect.StructField) string {
 			return field.Tag.Get("en_comment")
 		})
+	}
+}
+
+// 手机号码校验
+func MobileValidation(fl validator.FieldLevel) bool {
+	verificationRole := `^1[345789]\d{9}$`
+	field := fl.Field()
+
+	switch field.Kind() {
+	case reflect.String:
+		re, err := regexp.Compile(verificationRole)
+		if err != nil {
+			fmt.Println("规则校验存在错误：", err.Error())
+			return false
+		}
+
+		fmt.Println("规则校验结果：", re.MatchString(field.String()))
+
+		return re.MatchString(field.String())
+	default:
+		return false
 	}
 }
