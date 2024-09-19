9cbbad89df3f3511ad16ff136b0572f96417da24
dongfenglong <1024071953@qq.com>
2024-09-19 10:21:39 +0800

    删除旧版APP

--- App/config/index.js
+++
@@ -1,9 +0,0 @@
-// 根路径
-// export const base_api = 'http://192.168.25.149:8080'
-export const base_api = 'https://wmsx.api.goexc.cn'
-
-//图片域名
-export const oss_api = 'https://wms.file.goexc.cn/images/'
-
-//默认物料图片
-export const material_image = '/static/icons/material.svg'
